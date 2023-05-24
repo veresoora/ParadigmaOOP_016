46633913ea6e09a5231bb85ddffea9b431affe91
veresoora <lahanabira@gmail.com>
2023-05-24 18:16:36 +0700

    menambahkan method cetakAnak ke class ibu untuk di print ke daftar nama anak

--- mainIA/ibu.h
+++ mainIA/ibu.h
@@ -1,4 +1,4 @@
-#idndef IBU_H
+#ifndef IBU_H
 #define IBU_H
 
 class ibu {
@@ -12,9 +12,17 @@ public:
 	~ibu() {
 		cout << "Ibu \"" << nama << "\" tidak ada\n";
 	}
-	void tambahAnak(anak* pAnak);
+	void tambahAnak(anak*);
 	void cetakAnak();
 };
 void ibu::tambahAnak(anak* pAnak) {
 	daftar_anak.push_back(pAnak);
 }
+void ibu::cetakAnak() {
+	cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
+	for (auto& a : daftar_anak) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
+#endif
