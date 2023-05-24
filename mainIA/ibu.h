e70b05ad67bda867baed7d982d2276cd6dadd396
veresoora <lahanabira@gmail.com>
2023-05-24 17:58:16 +0700

    menambahkan method tambahAnak ke class Ibu untuk menambahkan instance ke class anak anak kedalam daftar anak

---
+++ mainIA/ibu.h
@@ -0,0 +1,17 @@
+#idndef IBU_H
+#define IBU_H
+
+class ibu {
+public:
+	string nama;
+	vector<anak*> daftar_anak;
+
+	ibu(string pNama) :nama(pNama) {
+		cout << "Ibu \"" << nama << "\" ada\n";
+	}
+	~ibu() {
+		cout << "Ibu \"" << nama << "\" tidak ada\n";
+	}
+	void tambahAnak(anak* pAnak);
+	void cetakAnak();
+};
