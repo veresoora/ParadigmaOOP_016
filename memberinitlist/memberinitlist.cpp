e3ec698aba7b5a25203a2f2e0ed70c5133b6595b
veresoora <lahanabira@gmail.com>
2023-05-24 19:58:37 +0700

    constructor public telah ditambahkan ke class mahasiswa

--- memberinitlist/memberinitlist.cpp
+++ memberinitlist/memberinitlist.cpp
@@ -7,4 +7,9 @@ private:
 	const int id;
 	string nama;
 	float nilai;
+
+public:
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) { //Member initialization List
+
+	}
 };
